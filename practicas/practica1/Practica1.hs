57fd512962535812d6ea9f155c9e6cdf12895555
Tania Rubí <taniarubi@ciencias.unam.mx>
2019-02-14 17:53:50 -0600

    Ya quedó la práctica

--- practicas/practica1/Practica1.hs
+++ practicas/practica1/Practica1.hs
@@ -1,19 +1,40 @@
 module Practica1 where
 
--- |1| Función getNth: 
+{- |1| Función getNth: Recibe un número y una lista. Regresa el n-ésimo 
+   elemento de la lista. 
+-}   
 getNth :: Int -> [a] -> a
-getNth = error "Implementar"
+getNth 0 (x:_) = x
+getNth _ [] = error "errror"
+getNth n (_:xs) 
+            | n<0 = error "Valor no valido"
+            | otherwise = getNth (n-1) xs 
 
 {- |2| Función update: Recibe un número, un elemento y una lista. Actualiza 
    el n-ésimo elemento de la lista.
 -}
 update :: Int -> a -> [a] -> [a]
-update = error "Implementar"
-                                                                                .
-{- |3| Función dropP: Se prohíbe el uso de las funciónes TAKE y DROP.
+update _ a [] = []
+update 0 a (x:xs) = (a:xs)
+update n a (x:xs) = (x:(update (n-1) a (xs)))
+
+{- |3| Función dropP: Recibe un número entero y uan lista. Regresa la lista
+   sin los elementos de cada n posición. 
+   Se prohíbe el uso de las funciónes TAKE y DROP.
 -}
 dropP :: Int -> [a] -> [a]
-dropP = error "Implementar"
+dropP n (x:xs) =  dropAux n n (x:xs)
+
+{-
+   Función dropAux : Recibe dos números y una lista. Borra el elemento en 
+   la m posición y luego cada n posición. Regresa la lista sin los elementos 
+   en la m posición y cada n posición.
+-}
+dropAux :: Int -> Int -> [a] -> [a]
+dropAux n m [] = []
+dropAux n m (x:xs)
+                | m == 1 = dropAux n n xs
+                | otherwise = x:(dropAux n (m - 1) xs)  
 
 {- |4| Función split: Recibe un número y una lista. Separa una lista en dos 
    partes, tomando en cuenta el entero dado. Se obtiene una lista de listas.
@@ -21,21 +42,39 @@ dropP = error "Implementar"
    Hint: Usar las funciones takeL y dropL vistas en el laboratorio.
 -}
 split :: Int -> [a] -> [[a]]
-split = error "Implementar"
+split _ [] = []
+split n (x:xs) = takeL n (x:xs) : split n (dropL n (x:xs)) 
 
--- |5| Función range: 
+{- |5| Función range: Recibe dos números enteros. Regresa los números que
+   están entre el rango de n y m. Si n y m son iguales, regresa la lista
+   con el número n. Si n es mayor a m, entonces se regresa una lista con n 
+   como cabeza y cada vez que entre la llamada recursiva se disminuye a n
+   en 1 para que el antecesor de n sea la nueva cabeza y así sucesivamente 
+   hasta llegar a m. Finalmente si n es menor a m, se regresa una lista con n
+   como cabeza y cada vez que entre la llamada recursiva se aumenta a n en 1 
+   para que el sucesor de n sea la nueva cabeza y así sucesivamente hasta 
+   llegar a m. 
+-}
 range :: Int -> Int -> [Int]
-range = error "Implementar"
+range n m 
+            | n == m = [n]
+            | n > m = n : range(n-1) m
+            | otherwise = n : range(n+1) m
 
 {- |6| Función filterP: Regresa los elementos de una lista que cumplen un 
    predicado.  Se prohíbe el uso de la función FILTER.
 -}
 filterP :: (a -> Bool) -> [a] -> [a]
-filterP = error "Implementar"
+filterP _ [] = []
+filterP t (x:xs) 
+            | t x = x : filterP t xs
+            | otherwise = filterP t xs
+
 
 {-|7| Función rotate: Rota n lugares de una lista.-}
 rotate :: Int -> [a] -> [a]
-rotate = error "Implementar"
+rotate 0 l = l
+rotate n (x:xs) = rotate (n-1) (xs ++ [x])
 
 {- |8| Función palindrome: Nos dice si una lista es palindroma o no. Por
    definición, una palabra palíndroma es la lista vacía y la lista con un
@@ -75,36 +114,64 @@ repite n a =
     if n < 0 then error "El número debe ser un entero positivo." 
     else (a:repite (n-1) a)
 
-{- |12| Función fib: Recibe un número y regresa la función fibonacci aplicada
-   al número.
+{- |12| Función fib: Recibe un número entero. Regresa la función fibonacci 
+   aplicada al número.
  -}
 fib :: Int -> Int
-fib = error "Implementar"
+fib 0 = 0
+fib 1 = 1
+fib n =
+    if n < 0 then error "El número debe ser un entero positivo."
+    else fib (n-1) + fib(n-2)
 
 -- Funciones auxiliares.
 
--- Función headL: Regresa el primer elemento de una lista.
+-- Función isEmpty: Recibe una lista. Nos dice si la lista es la lista vacía.
+isEmpty :: [a] -> Bool
+isEmpty [] = True
+isEmpty _ = False
+
+{- Función takeL: Recibe un número entero y una lista. Toma los primeros n 
+   elementos de una lista.
+-}
+takeL :: Int -> [a] -> [a]
+takeL  0 _ = []
+takeL _ [] =[]
+takeL n (x:xs) = x:takeL (n-1) xs
+
+{- Función dropL: Recibe un número entero y una lista. Elimina los primeros n 
+   elementos de una lista.
+-}
+dropL :: Int -> [a] -> [a]
+dropL _ [] = []
+dropL 0 (x:xs) = x:xs  
+dropL n (x:xs) = dropL (n-1) xs  
+
+-- Función headL: Recibe una lista y regresa el primer elemento de la lista.
 headlL :: [a] -> a
-headlL [] = error "Lista vacía."
+headlL [] = error "La lista está vacía."
 headlL (x:xs) = x
 
--- Función lastL: Regresa el úlitmo elemento de una lista.
+-- Función lastL: Recibe una lista y regresa el úlitmo elemento de la lista.
 lastL :: [a] -> a
-lastL [] = error " "
+lastL [] = error "La lista está vacía."
 lastL (x:[]) = x
 lastL (x:xs) = lastL xs 
 
--- Función tailL: Regresa la cola de una lista.
+-- Función tailL: Recibe una lista y regresa la cola de la lista.
 tailL :: [a] -> [a]
 tailL [] = error ""
 tailL (x:xs) = xs 
 
--- Función initL: Regresa la lista sin el último elemento.
+{- Función initL: Recibe una lista. Si la lista recibida es vacía, entonces
+   regresa la lista vacía. En caso contrario, regresa la lista sin el último
+   elemento. 
+-}
 initL :: [a] -> [a]
 initL [] = error " D="
 initL (x:xs) = if (isEmpty xs) then [] else x:(initL xs)
 
--- Función isEmpty: Nos dice si una lista es la lista vacía.
-isEmpty :: [a] -> Bool
-isEmpty [] = True
-isEmpty _ = False
+-- Función lengthL: Recibe una lista y regresa el tamaño de la lista.
+lengthL :: [a] -> Int
+lengthL [] = 0
+lengthL (x:xs) = 1 + lengthL (xs)
--- practicas/practica1/README.txt
+++ practicas/practica1/README.txt
@@ -1,8 +1,10 @@
 Integrantes del equipo:
 
-1.
+1. López Pérez Frida Fernanda 
+   #cuenta : 315110520.
 
 2. Rubí Rojas Tania Michelle
    # cuenta: 315121719.
 
-3.  
+3. Sánchez Rangel Joanna Lizeth
+   # cuenta : 315060982.
