640d7cee36a9e4df76e22e57c81c7aac836ae241
Tania Rubí <taniarubi@ciencias.unam.mx>
2019-03-17 03:01:45 -0600

    avance del proyecto1

--- proyectos/proyecto1/LogicaProp.hs
+++ proyectos/proyecto1/LogicaProp.hs
@@ -1,90 +1,108 @@
+-- @autor Rubí Rojas Tania Michelle.
 module LogicaProp where
+import Data.List
 
 -- Las variables proposicionales son del tipo Char.
 type VarP = Char
 
--- El tipo de dato de las fórmulas proposicionales.
-data Prop = Top | Bot | Var VarP | Neg Prop | Conj Prop Prop | Disy Prop Prop 
-            | Impl Prop Prop | Syss Prop Prop 
+-- El tipo de dato para las fórmulas proposicionales.
+data Prop = Var VarP  -- Var 'p'
+          | Neg Prop   -- ~P
+          | Conj Prop Prop -- P ^ Q
+          | Disy Prop Prop -- P v Q
+          | Impl Prop Prop -- P -> Q
+          | Syss Prop Prop -- P <-> Q
             deriving (Eq, Ord, Show)
 
-{- Los estados son listas de tuplas tal que su primer componente es el nombre
-   de una variable proposicional y su segundo componente es un valor booleano.
+{- Los estados son listas de tuplas donde la primer componente de la tupla es 
+   una variable proposicional y su segundo componente será el valor asociado a 
+   dicha variable.
 -}
 type Estado = [(VarP, Bool)]
 
-{- Recibe una expresión proposicional (phi) y un estado e. Regresa la 
-   interpretación de la expresión proposicional con el estado dado. 
+-- Ejemplos de variables proposicionales:
+x, y, z :: VarP 
+x = 'x'
+y = 'y'
+z = 'z'
+
+-- Ejemplos de fórmulas:
+alpha, gamma, taf :: Prop
+alpha = (Conj (Var x) (Var y))
+gamma = (Impl (Conj (Var z) (Var y)) (Disy (Var y) (Var z)))
+taf = (Disy (Neg (Var x)) (Neg (Var y)))
+
+{- |1. Función interp|  Recibe una fórmula (phi) y un estado e. Regresa la 
+   interpretación de phi con el estado dado.
+   Ejemplo:
+   *Main> interp (Conj (Var 'p') (Var 'q')) [('p', True), ('q', False)]
+   False
 -}
 interp :: Prop -> Estado -> Bool
 interp phi e = case phi of 
-    Top -> True
-    Bot -> False 
-    Var i -> busca i e
+    Var i -> buscaBool i e
     Neg p -> not (interp p e)
-    Conj p q -> interp p e && interp q e
-    Disy p q -> interp p e || interp q e
-    Impl p q -> not (interp p e) || interp q e
-    Syss p q -> interp p e == interp q e
+    Conj p q -> (interp p e) && (interp q e)
+    Disy p q -> (interp p e) || (interp q e)
+    Impl p q -> (not (interp p e)) || (interp q e)
+    Syss p q -> (interp p e) == (interp q e)
 
-{- Regresa la lista de variables proposicionales que figuran en (phi), 
-   sin repetición.
+{- |2. Función vars| Recibe una formula (phi). Regresa la lista de variables 
+   proposicionales que figuran en (phi), sin repetición.
+   Ejemplo: 
+   *Main> vars (Syss (Impl (Var 'p') (Var 'r')) (Conj (Var 'q') (Var 's')))
+   "prqs"
 -}
 vars :: Prop -> [VarP]
 vars phi = case phi of
-    Top -> []
-    Bot -> []
-    Var i-> [i]
+    Var x -> [x]
     Neg p -> vars p
-    Conj p q -> vars p ++ vars q
-    Disy p q -> vars p ++ vars q
-    Impl p q -> vars p ++ vars q
-    Syss p q -> vars p ++ vars q
+    Conj p q -> vars p `union` vars q
+    Disy p q -> vars p `union` vars q
+    Impl p q -> vars p `union` vars q
+    Syss p q -> vars p `union` vars q
 
-{- Recibe una fórmula (phi) con n-variables proposicionales. Regresa la lista
-   con los 2^n estados distintos para (phi).
+{- |3. Función estados| Recibe una fórmula (phi) con n-variables 
+   proposicionales. Regresa la lista con los 2^n estados distintos para (phi).
+   Ejemplo:
+   *Main> estados (Impl (Neg (Var 'q')) (Disy (Var 'r') (Var 'p')))
+   [[('q',True),('r',True),('p',True)],[('q',True),('r',True),('p',False)],
+   [('q',True),('r',False),('p',True)],[('q',True),('r',False),('p',False)],
+   [('q',False),('r',True),('p',True)],[('q',False),('r',True),('p',False)],
+   [('q',False),('r',False),('p',True)],[('q',False),('r',False),('p',False)]]
 -}
-estados :: Prop -> [Estado]
+estados:: Prop -> [Estado]
 estados phi = subconj (vars phi)
-   where 
-    subconj [] = [[]]
-    subconj (x:xs) = 
-        [(x, True) : i | i <- subconj xs] ++ [(x, False) : i | i <- subconj xs] 
+    where subconj [] = [[]]
+          subconj (x:xs) = 
+            [(x,True):i | i <- subconj xs] ++ [(x,False):i | i <- subconj xs] 
 
-
-{- Recibe una fórmula proposicional (phi). Regresa la lista con todos aquellos
-   estados I tales que I(phi) = 1.
+{- |4. Función varCN} Recibe una fórmula (phi). Regresa la lista de variables 
+   proposicionales que figuran en (phi). La diferencia con la función vars es
+   que si la variable porposicional tiene una negación, la manda a la lista 
+   junto con su conectivo unario.
+   Ejemplo: 
+   *Main> varCN (Conj (Neg (Var 'p')) (Var 'r'))
+   [Neg (Var 'p'),Var 'r']
 -}
-modelos:: Prop -> [Estado]
-modelos phi = [i | i <- estados phi, interp phi i == True]
+varCN :: Prop -> [Prop]
+varCN phi = case phi of
+    Var x -> [Var x]
+    (Neg (Var i)) -> [Neg (Var i)]
+    Neg p -> varCN p
+    Conj p q -> varCN p `union` varCN q
+    Disy p q -> varCN p `union` varCN q
+    Impl p q -> varCN p `union` varCN q
+    Syss p q -> varCN p `union` varCN q
 
--- Nos dice si una fórmula proposicional (phi) es una tautología.
-tautologia :: Prop -> Bool 
-tautologia phi = estados phi == modelos phi
+-- Funciones auxiliares. --
 
-{- Nos dice si una fórmula proposicional (phi) es satisfacible en una 
-   interpretación.
+{- |1. Función auxiliar buscaBool| Recibe una variable proposicional p, y un
+   estado [(p,b)]. Regresa la segunda componente del primer par ordenado de
+   la lista de estados I, cuyo primer componente sea igual a la variable p.
+   Ejemplo:
+   *Main> 'r' [('p', True), ('q', True), ('r', False)]
+   False
 -}
-satisfen :: Prop -> Estado -> Bool 
-satisfen phi e = interp phi e == True
-
--- Nos dice si una fórmula proposicional (phi) es satisfacible.
-satisf :: Prop -> Bool
-satisf phi = modelos phi /= []
-
--- Nos dice si una fórmula proposicional (phi) es insatisfacible.
-insatisfen :: Prop -> Estado -> Bool 
-insatisfen phi e = interp phi e == False
-
--- Nos dice si una fórmula proposicional (phi) es una contradicción.
-contrad :: Prop -> Bool 
-contrad phi = modelos phi == []
-
--- Funciones auxiliares.
-
-{- Recibe una variable proposicional v, y un estado e = [(v,b)]. Regresa 
-   la segunda entrada del primer par ordenado de la lista de estados l,
-   cuya primer entrada sea igual a la variable v,
--}
-busca :: (Eq v) => v -> [(v,b)] -> b 
-busca v l = head [b | (x,b) <- l, v == x]
+buscaBool :: (Eq p) => p -> [(p,b)] -> b 
+buscaBool p e = head [b | (x,b) <- e, p == x]
--- proyectos/proyecto1/Proyecto1.hs
+++ proyectos/proyecto1/Proyecto1.hs
@@ -1,4 +1,6 @@
+-- @autor Rubí Rojas Tania Michelle.
 import LogicaProp
+import Data.List
 
 -- Las variables proposicionales de las declaraciones de los sospechosos.
 p, q, r :: VarP
@@ -10,7 +12,7 @@ q = 'q'
 r = 'r'
 
 -- Las fórmulas proposicionales de las declaraciones de los sospechosos.
-phi, varphi, psi, declaracion:: Prop
+phi, varphi, psi, declaracionFinal :: Prop
 -- Proposición phi: Lo que dijo el esposo.
 phi = (Conj (Neg (Var p)) (Neg (Var r)))
 -- Proposición varphi: Lo que dijo el mayordomo. 
@@ -18,13 +20,40 @@ varphi = (Conj (Neg (Var p)) (Var q))
 -- Proposición psi: Lo que dijo el amante.
 psi = (Conj (Neg (Var q)) (Var p))
 -- Declaración final de los sospechosos:
-declaracion = (Conj phi (Conj varphi psi))
+declaracionFinal = (Conj phi (Conj varphi psi))
+
+modelos :: Prop -> [Estado]
+modelos phi = [i | i <- estados declaracionFinal, interp phi i == True]
+
+noModelos :: Prop -> [Estado]
+noModelos phi = [i | i <- estados declaracionFinal, interp phi i == False]
 
 -- Final del juicio.
-juicio :: Prop -> [Estado]
-juicio lll = estados (Conj phi varphi)
+especificacion :: Prop -> [Estado]
+especificacion declaracion = 
+    [i | i <- interseccion (modelos phi) (noModelos varphi) (noModelos psi)]
+    ++ [i | i <- interseccion (modelos varphi) (noModelos phi) (noModelos psi)]
+    ++ [i | i <- interseccion (modelos psi) (noModelos phi) (noModelos varphi)]
 
--- |5| Función isSubSet: Recibe dos listas y nos dice si la primera lista es subconjunto de la segunda. 
-isSubSet :: Eq a => [a] -> [a] -> Bool
-isSubSet [] _ = True 
-isSubSet (x:xs) ys = elem x ys && isSubSet xs ys
+juicio:: Prop -> [Estado]
+juicio declaracion =
+    [i | i <- especificacion declaracion, 
+    (diferentes phi i == True && iguales varphi i == True) && 
+    (iguales varphi i == True && iguales psi i == True)
+    || (iguales phi i == True && diferentes varphi i == True) && 
+    (diferentes varphi i == True && iguales psi i == True)
+    || (iguales phi i == True && iguales varphi i == True) && 
+    (iguales varphi i == True && diferentes psi i == True)]
+
+
+interseccion :: Eq a => [a] -> [a] -> [a] -> [a]
+interseccion [] [] [] = [] 
+interseccion xs ys zs = intersect (intersect xs ys) zs
+
+diferentes :: Prop -> Estado -> Bool
+diferentes beta e = 
+    (interp (head (varCN beta)) e) /= (interp (last (varCN beta)) e)
+
+iguales :: Prop -> Estado -> Bool
+iguales beta e = 
+    (interp (head (varCN beta)) e) == (interp (last (varCN beta)) e)
