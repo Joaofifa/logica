ad78faa9e659e3449424121248fed175e731af5b
Tania Rubí <taniarubi@ciencias.unam.mx>
2019-03-23 23:12:06 -0600

    Proyecto1: Función LogicaProp terminada en latex y haskell.

--- proyectos/proyecto1/LogicaProp.hs
+++ proyectos/proyecto1/LogicaProp.hs
@@ -5,6 +5,12 @@ import Data.List
 -- Las variables proposicionales son del tipo Char.
 type VarP = Char
 
+{- Los estados son listas de tuplas donde la primer componente de la tupla es 
+   una variable proposicional y su segundo componente será el valor asociado a 
+   dicha variable.
+-}
+type Estado = [(VarP, Bool)]
+
 -- El tipo de dato para las fórmulas proposicionales.
 data Prop = Var VarP  -- Var 'p'
           | Neg Prop   -- ~P
@@ -12,13 +18,7 @@ data Prop = Var VarP  -- Var 'p'
           | Disy Prop Prop -- P v Q
           | Impl Prop Prop -- P -> Q
           | Syss Prop Prop -- P <-> Q
-            deriving (Eq, Ord, Show)
-
-{- Los estados son listas de tuplas donde la primer componente de la tupla es 
-   una variable proposicional y su segundo componente será el valor asociado a 
-   dicha variable.
--}
-type Estado = [(VarP, Bool)]
+          deriving (Eq, Ord, Show)
 
 -- Ejemplos de variables proposicionales:
 x, y, z :: VarP 
@@ -34,9 +34,19 @@ taf = (Disy (Neg (Var x)) (Neg (Var y)))
 
 {- |1. Función interp|  Recibe una fórmula (phi) y un estado e. Regresa la 
    interpretación de phi con el estado dado.
-   Ejemplo:
+   Las definiciones que usamos para cada una es la aplicación directa de
+   nuestra definición de semántica. Notemos dos cosas, la primera, que se 
+   utilizó una equivalencia lógica para la interpretación de la implicación,
+   y segundo, que se hace uso de una función auxiliar 'buscaBool' cuyo
+   objetivo es regresar el valor booleano asociado a la primer aparición de
+   la variable proposicional varP que se le pase como parámetro.
+
+   Ejemplos:
    *Main> interp (Conj (Var 'p') (Var 'q')) [('p', True), ('q', False)]
    False
+
+   *Main> interp (Disy (Neg (Var x)) (Neg (Var y))) [('x', False), ('y', True)]
+   True
 -}
 interp :: Prop -> Estado -> Bool
 interp phi e = case phi of 
@@ -49,9 +59,18 @@ interp phi e = case phi of
 
 {- |2. Función vars| Recibe una formula (phi). Regresa la lista de variables 
    proposicionales que figuran en (phi), sin repetición.
-   Ejemplo: 
+
+   Aquí simplemente utilizamos la función 'union' para unir las listas finales
+   que contienen las variables proposicionales de cada una de las fórmulas. 
+   Por cómo está definida 'union', nos regresa la lista que contiene todos 
+   los elementos de las listas que recibe, sin repetición.
+
+   Ejemplos: 
    *Main> vars (Syss (Impl (Var 'p') (Var 'r')) (Conj (Var 'q') (Var 's')))
    "prqs"
+
+   *Main vars (Disy (Neg (Var x)) (Neg (Var y)))
+   "xy"
 -}
 vars :: Prop -> [VarP]
 vars phi = case phi of
@@ -63,14 +82,27 @@ vars phi = case phi of
     Syss p q -> vars p `union` vars q
 
 {- |3. Función estados| Recibe una fórmula (phi) con n-variables 
-   proposicionales. Regresa la lista con los 2^n estados distintos para (phi).
-   Ejemplo:
+   proposicionales. Regresa la lista de listas con los 2^n estados 
+   distintos para (phi).
+
+   Notemos que para obtener los 2^n estados posibles, debemos obtener el 
+   subconjunto de listas de la lista de variables proposicionales de 
+   (phi), hacer las combinaciones posibles entre los estados y los valores 
+   booleanos e ir concatenando las parejas de tuplas para formar una lista
+   (que es justo lo que hacemos en la función subconj). Y como subconj trabaja
+   con una lista de comprensión, al final obtendremos una lista de listas 
+   (ya que los estados son listas) con los 2^n estados.
+   
+   Ejemplos:
    *Main> estados (Impl (Neg (Var 'q')) (Disy (Var 'r') (Var 'p')))
    [[('q',True),('r',True),('p',True)],[('q',True),('r',True),('p',False)],
    [('q',True),('r',False),('p',True)],[('q',True),('r',False),('p',False)],
    [('q',False),('r',True),('p',True)],[('q',False),('r',True),('p',False)],
    [('q',False),('r',False),('p',True)],[('q',False),('r',False),('p',False)]]
--}
+
+   *Main> estados (Disy (Neg (Var x)) (Neg (Var y)))
+   [[('x',True),('y',True)],[('x',True),('y',False)],[('x',False),('y',True)],[('x',False),('y',False)]]
+   -}
 estados:: Prop -> [Estado]
 estados phi = subconj (vars phi)
     where subconj [] = [[]]
@@ -78,12 +110,23 @@ estados phi = subconj (vars phi)
             [(x,True):i | i <- subconj xs] ++ [(x,False):i | i <- subconj xs] 
 
 {- |4. Función varCN} Recibe una fórmula (phi). Regresa la lista de variables 
-   proposicionales que figuran en (phi). La diferencia con la función vars es
-   que si la variable porposicional tiene una negación, la manda a la lista 
-   junto con su conectivo unario.
-   Ejemplo: 
+   proposicionales que figuran en (phi) junto con su conectivo unario (si es
+   que tiene).
+   
+   La diferencia con la función vars es que si la variable porposicional tiene
+   una negación, la manda a la lista junto con su conectivo unario.
+
+   Aquí simplemente utilizamos la función 'union' para unir las listas finales
+   que contienen las variables proposicionales de cada una de las fórmulas. 
+   Por cómo está definida 'union', nos regresa la lista que contiene todos 
+   los elementos de las listas que recibe, sin repetición.
+
+   Ejemplos: 
    *Main> varCN (Conj (Neg (Var 'p')) (Var 'r'))
    [Neg (Var 'p'),Var 'r']
+
+   *Main> varCN (Neg (Conj (Var 'p') (Neg (Var 'q'))))
+   [Var 'p',Neg (Var 'q')]
 -}
 varCN :: Prop -> [Prop]
 varCN phi = case phi of
@@ -98,12 +141,17 @@ varCN phi = case phi of
 {- |5. Función sonDiferentes| Recibe una fórmula (phi) con únicamente dos
    variables proposicionales y un estado e. Nos dice si la interpretación de
    cada una de sus variables con el estado e son diferentes.
+
    En esta función es donde 'varCN' cobra mucha importancia. Sabemos que las 
    variables proposicionales de (phi) pueden estar solas o negadas, por lo que
    no nos sirve usar la función 'vars' ya que no estaríamos considerando el 
    caso en que las variables están negadas. Así, utilizamos la función 'varCN' 
    para evaluar ambas variables de la fórmula (phi) y verificar si la 
-   interpretación de éstas es diferente o no.
+   interpretación de éstas es diferente o no. Esta función es fácil de 
+   realizar ya que únicamente hay que checar si la interpretación de la
+   primer variable de (phi) es diferente a la interpretación de la segunda
+   variable de (phi), con el estado dado.
+
    Ejemplos:
    *Main> sonDiferentes (Conj (Var 'p') (Neg (Var 'q'))) 
     [('p', True), ('q', False)]
@@ -121,15 +169,20 @@ sonDiferentes :: Prop -> Estado -> Bool
 sonDiferentes phi e = 
     (interp (head (varCN phi)) e) /= (interp (last (varCN phi)) e)
 
-{- |5. Función sonIguales| Recibe una fórmula (phi) con únicamente dos 
+{- |6. Función sonIguales| Recibe una fórmula (phi) con únicamente dos 
    variables proposicionales y un estado e. Nos dice si la interpretación de
    cada una de sus variables con el estado e son iguales.
-   En esta función es donde 'varCN' cobra mucha importancia. Sabemos que las 
+
+   EEn esta función es donde 'varCN' cobra mucha importancia. Sabemos que las 
    variables proposicionales de (phi) pueden estar solas o negadas, por lo que
    no nos sirve usar la función 'vars' ya que no estaríamos considerando el 
    caso en que las variables están negadas. Así, utilizamos la función 'varCN' 
    para evaluar ambas variables de la fórmula (phi) y verificar si la 
-   interpretación de éstas es igual o no.
+   interpretación de éstas es igual o no. Esta función es fácil de 
+   realizar ya que únicamente hay que checar si la interpretación de la
+   primer variable de (phi) es igual a la interpretación de la segunda
+   variable de (phi), con el estado dado.
+
    Ejemplos:
    *Main> sonIguales (Conj (Var 'p') (Neg (Var 'q'))) 
    [('p', True), ('q', False)]
@@ -149,12 +202,18 @@ sonIguales beta e =
 
 -- Funciones auxiliares. --
 
-{- |1. Función auxiliar buscaBool| Recibe una variable proposicional p, y un
-   estado [(p,b)]. Regresa la segunda componente del primer par ordenado de
-   la lista de estados I, cuyo primer componente sea igual a la variable p.
-   Ejemplo:
-   *Main> 'r' [('p', True), ('q', True), ('r', False)]
+{- |1. Función auxiliar buscaBool| Recibe una variable proposicional varP, y 
+   un estado [(varP,Bool)]. Regresa la segunda componente del primer par 
+   ordenado de la lista de estados I, cuyo primer componente sea igual a la
+   variable varP. Es decir, regresa el valor booleano asociado a la primer 
+   variable proposicional varP que encuentre.
+
+   Ejemplos:
+   *Main> buscaBool 'r' [('p', True), ('q', True), ('r', False)]
    False
+
+   *Main> buscaBool 'x' [('l', False), ('x', True), ('m', True), ('s', True)]
+   True
 -}
-buscaBool :: (Eq p) => p -> [(p, Bool)] -> Bool
-buscaBool p e = head [b | (x,b) <- e, p == x]
+buscaBool :: (Eq varP) => varP -> [(varP, Bool)] -> Bool
+buscaBool varP e = head [b | (x,b) <- e, varP == x]
