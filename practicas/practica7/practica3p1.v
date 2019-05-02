eb214c644082c0727526384fe54cac450855a983
Tania Rubí <taniarubi@ciencias.unam.mx>
2019-05-02 10:16:39 -0500

    Tarea-examen terminada.

--- practicas/practica7/practica3p1.v
+++ practicas/practica7/practica3p1.v
@@ -7,26 +7,41 @@ match x with
 end
 end.
 
-(*Ejercicio 1*
-Fixpoint suma (x y : nat) : nat :=
-AQUI VA SU CÓDIGO
-*)
+(* Probando par
+Compute par 10. *)
 
-(*EJEMPLO PRODUCTO DE DOS NATURALES.
-  PUEDEN DES-COMENTARLO HASTA QUE HAYAN HECHO suma
+(*Ejercicio 1*)
+Fixpoint suma (x y : nat) : nat :=
+match x with
+| 0 => y
+| S n => S (suma n y)
+end.
+
+(* Probando suma 
+Compute suma 15 5.*)
+
+(*EJEMPLO PRODUCTO DE DOS NATURALES.*)
 Fixpoint prod (x y : nat) : nat :=
 match x with
 | 0 => 0
 | S n => suma y (prod n y)
 end.
+ 
+(* Probando prod
+Compute prod 2 5.*)
 
-Compute prod 2 5.
-*)
-
-(*Ejercicio 2*
+(*Ejercicio 2*)
 Fixpoint odd (x:nat) : Prop :=
-AQUI VA SU CÓDIGO
-*)
+match x with
+| 0 => False
+| S n => match n with
+| 0 => True
+| S m => odd m
+end
+end.
+
+(* Probando odd
+Compute odd 101.*)
 
 (*Ejercicio 3*)
 Theorem proove_false_x_x : ~False.
@@ -56,19 +71,48 @@ Qed.
 
 (*Ejercicio 5*)
 Lemma neutro_aditivo_derecha : forall n:nat, n+0 = n.
-Admitted.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
 
 (*Ejercicio 6*)
 Lemma neutro_mult : forall n:nat, (S 0)*n = n.
+Proof.
+intros.
+simpl.
+rewrite -> neutro_aditivo_derecha.
+reflexivity.
+Qed.
 
-Admitted.
 (*Ejercicio 7*)
 Lemma neutro_mult_der : forall n:nat, n*(S 0) = n.
-Admitted.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
 
 (*Ejercicio 8*)
 Lemma suma_uno : forall n:nat, n + 1 = S n.
-Admitted.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
 
 Lemma mult_cero : forall n : nat, 0 * n = 0.
 Proof.
@@ -79,12 +123,38 @@ Qed.
 
 (*Ejercicio 9*)
 Lemma mult_cero_derecha : forall n : nat, n * 0 = 0.
-Admitted.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
 
 (*Ejercicio 10*)
 Lemma sucesor_suma : forall n m : nat, n + S m = S (n + m).
-Admitted.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
 
 (*Ejercicio 11*)
 Theorem conm_suma: forall n m: nat, n+m = m+n.
-Admitted.
+Proof.
+intros.
+induction n.
+simpl.
+rewrite -> neutro_aditivo_derecha.
+reflexivity.
+simpl.
+rewrite IHn.
+rewrite -> sucesor_suma.
+reflexivity.
+Qed.
---
+++ practicas/practica8/practica3p2.v
@@ -0,0 +1,192 @@
+(* Auxiliares. *)
+Lemma neutro_aditivo_derecha : forall n:nat, n+0 = n.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
+
+Lemma sucesor_suma : forall n m : nat, n + S m = S (n + m).
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
+
+Theorem conm_suma: forall n m: nat, n+m = m+n.
+Proof.
+intros.
+induction n.
+simpl.
+rewrite -> neutro_aditivo_derecha.
+reflexivity.
+simpl.
+rewrite IHn.
+rewrite -> sucesor_suma.
+reflexivity.
+Qed.
+
+(*Ejercicio 1*)
+Theorem asoc_suma : forall n m p:nat, (n+m)+p = n+(m+p).
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
+
+(*Ejercicio 2*)
+Lemma distr_mult_y_suma : forall n m p:nat, n*(m+p) = n*m + n*p.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+rewrite -> asoc_suma with m (n * m) (p + n * p).
+rewrite -> asoc_suma.
+rewrite <- asoc_suma with p (n * m) (n * p).
+rewrite -> conm_suma with p (n * m).
+rewrite <- asoc_suma with (n * m) p (n * p).
+reflexivity.
+Qed.
+
+(*Ejercicio 3*)
+Theorem menor_igual_cero : forall n:nat, 0 <= n.
+Proof.
+intros.
+induction n.
+constructor.
+constructor.
+trivial.
+Qed.
+
+Require Import List.
+
+Notation "x :: l" := (cons x l) (at level 60, right associativity).
+Notation "[]" := nil.
+Notation "[ x , .. , y ]" := (cons x .. (cons y nil) .. ).
+
+Variables A : Type.
+
+Fixpoint elem (z:A) (l: list A) :=
+  match l with
+    | nil => False
+    | (x::xs) => x = z \/ elem z xs
+  end.
+
+(*Ejercicio 4*)
+Theorem theorem_elem : forall (a:A) (l:list A), elem a (a::l).
+Proof.
+intros.
+simpl.
+intuition.
+Qed.
+
+(*Ejercicio 5*)
+Theorem theorem_elem2 : forall a:A, ~ elem a nil.
+Proof.
+intros.
+unfold not.
+simpl.
+trivial.
+Qed.
+
+(*Ejercicio 6*)
+Theorem theorem_elem3 : forall (a b:A) (l:list A), elem b l -> elem b (a::l).
+Proof.
+intros.
+simpl.
+induction l.
+simpl.
+intuition.
+simpl.
+intuition.
+Qed.
+
+(*Ejercicio 7*)
+Lemma nil_or_not : forall l:list A, l = [] \/ l <> [].
+Proof.
+intros.
+case l.
+intuition.
+intros.
+unfold not.
+right.
+intros.
+discriminate H.
+Qed.
+
+(*Ejercicio 8*)
+Theorem split : forall l:list A, l <> [] -> exists x:A, exists l1 l2: list A, l = l1 ++ x::l2.
+Proof.
+intros.
+destruct l.
+intuition.
+exists a.
+exists l.
+rewrite H.
+
+Qed.
+
+(*Ejercicio 9*)
+Lemma equal_lists : forall (a:A) (xs ys:list A), a::xs = a::ys <-> xs = ys.
+Proof.
+intros.
+induction xs.
+intuition.
+inversion H.
+reflexivity.
+inversion H.
+reflexivity.
+intuition.
+inversion H1.
+reflexivity.
+inversion H1.
+reflexivity.
+Qed.
+
+Variable p : A -> bool.
+
+(*Ejercicio 10*)
+Lemma diff : forall x y, p x = true -> p y = false -> x <> y.
+Proof.
+intros.
+unfold not.
+intros.
+symmetry in H1.
+rewrite H1 in H0.
+rewrite H in H0.
+intuition.
+Qed.
+
+Inductive bit: Type := 
+  | zero:bit
+  | one:bit.
+
+Definition bin := list bit.
+
+Check bit_ind.
+Print bin.
+
+(*Ejercicio 11*
+Fixpoint increment_one (b:bin):bin :=
+AQUI VA SU CÓDIGO
+*)
+
+(*Ejercicio 12*
+Fixpoint to_binary (n:nat):bin :=
+AQUI VA SU CÓDIGO
+*)
